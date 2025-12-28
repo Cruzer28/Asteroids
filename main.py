48276e2e10662208450fb8825fd5a150c46e5831
Cruzer28 <cruzer282@gmail.com>
2025-12-27 19:08:32 -0600

    Add containers + forward/back movement

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
     while True:
@@ -26,8 +30,10 @@ def main():
 
         screen.fill("black")
 
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+
+        for thing in drawable:
+            thing.draw(screen)
 
         pygame.display.flip()
 
