71a64f7f493493e632b720bb4e17e954d19ec949
Cruzer28 <cruzer282@gmail.com>
2025-12-27 20:35:09 -0600

    Add player shooting

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from logger import log_state
 from player import *
 from asteroid import *
 from asteroidfield import *
+from shot import *
 from logger import log_event
 
 def main():
@@ -23,10 +24,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, drawable, updatable)
 
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     asteroidfield = AsteroidField()
